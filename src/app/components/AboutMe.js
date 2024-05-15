790cf597f37dafaf2625dcbbb4a8990ea582a2a6
Chinmaya Kumar Mishra <103204520+Chinmaya0011@users.noreply.github.com>
2024-05-16 00:02:49 +0530

    Update AboutMe.js

--- src/app/components/AboutMe.js
+++ src/app/components/AboutMe.js
@@ -1,4 +1,5 @@
-"use client"
+"use client"; // Ensure this is at the top
+
 import React, { useState, useEffect, useRef } from 'react';
 import { useTheme } from './theme.js';
 
@@ -28,14 +29,26 @@ const AboutMe = () => {
 
   return (
     <div className={`section1 hero min-h-screen flex flex-col justify-center items-center px-4 ${isDarkMode ? 'dark' : ''}`}>
-      <div ref={containerRef} className='justify-center items-center px-4 text-center' style={{ width: containerWidth ? (containerWidth >= 568 ? '60%' : '90%') : 'auto' }}>
+      <div
+        ref={containerRef}
+        className="justify-center items-center px-4 text-center"
+        style={{ width: containerWidth ? (containerWidth >= 568 ? '60%' : '90%') : 'auto' }}
+      >
         <h1 className={`text-3xl font-bold mb-4 ${isDarkMode ? 'text-white' : ''}`}>CHINMAYA KUMAR MISHRA</h1>
         <p className={`text-lg mb-4 ${isDarkMode ? 'text-gray-300' : ''}`}>Full-Stack Web Developer</p>
         <p className={`mb-4 ${isDarkMode ? 'text-gray-300' : ''}`}>imchinu17@gmail.com | 7077835119 | Bhubaneswar, Odisha</p>
-        <p className={`mb-4 ${isDarkMode ? 'text-gray-300' : ''}`}>Passionate Full-Stack Web Developer based in Bhubaneswar, Odisha. Experienced in building innovative web solutions using React.js, Next.js, Node.js, and MongoDB. Skilled in developing user-friendly applications that meet the needs of clients and users alike.</p>
-        <p className={`mb-4 ${isDarkMode ? 'text-gray-300' : ''}`}>In my journey as a developer, I&apos;ve had the opportunity to work on various projects ranging from e-commerce platforms to hospital management systems. I thrive in collaborative environments and enjoy bringing creative ideas to life through code.</p>
-        <p className={`mb-4 ${isDarkMode ? 'text-gray-300' : ''}`}>I am deeply committed to continuous learning and staying up-to-date with the latest technologies and industry trends. I believe in the power of technology to drive positive change and am passionate about leveraging my skills to create meaningful solutions.</p>
-        <p className={`${isDarkMode ? 'text-gray-300' : ''}`}> Outside of coding, I enjoy exploring new technologies, reading tech blogs, and participating in hackathons. I also love to travel, discover new cuisines, and spend time with family and friends.</p>
+        <p className={`mb-4 ${isDarkMode ? 'text-gray-300' : ''}`}>
+          Passionate Full-Stack Web Developer based in Bhubaneswar, Odisha. Experienced in building innovative web solutions using React.js, Next.js, Node.js, and MongoDB. Skilled in developing user-friendly applications that meet the needs of clients and users alike.
+        </p>
+        <p className={`mb-4 ${isDarkMode ? 'text-gray-300' : ''}`}>
+          In my journey as a developer, I&apos;ve had the opportunity to work on various projects ranging from e-commerce platforms to hospital management systems. I thrive in collaborative environments and enjoy bringing creative ideas to life through code.
+        </p>
+        <p className={`mb-4 ${isDarkMode ? 'text-gray-300' : ''}`}>
+          I am deeply committed to continuous learning and staying up-to-date with the latest technologies and industry trends. I believe in the power of technology to drive positive change and am passionate about leveraging my skills to create meaningful solutions.
+        </p>
+        <p className={`${isDarkMode ? 'text-gray-300' : ''}`}>
+          Outside of coding, I enjoy exploring new technologies, reading tech blogs, and participating in hackathons. I also love to travel, discover new cuisines, and spend time with family and friends.
+        </p>
       </div>
     </div>
   );
